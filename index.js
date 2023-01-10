cebb12bbd78ee3a2d9a75d9fec2cbc1358a24f9d
vatchans <112924038+vatchans@users.noreply.github.com>
2023-01-10 19:35:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const http=require('http')
 const fs=require('fs')
 const server=http.createServer((req,res)=>{
     let timestamp=new Date()
-    let a=Math.floor(Math.random()*10)
-    fs.writeFileSync(`Date-time/date-time-${a}.txt`,`<h2>${timestamp}</h2>`,`utf-8`)
-  let currenttime=fs.readFileSync(`./Date-time/date-time-${a}.txt`,`utf-8`)
+    let a=Math.floor(Math.random()*100)
+    fs.writeFileSync(`date-time-${a}.txt`,`<h2>${timestamp}</h2>`,`utf-8`)
+  let currenttime=fs.readFileSync(`date-time-${a}.txt`,`utf-8`)
     res.writeHead(200,{'Content-type':'text/html'})
 
     res.end(currenttime)
 })
 const PORT = process.env.PORT || 8000;
-server.listen(PORT,()=>{console.log(`Server listening to ${PORT}`)})
+server.listen(PORT,()=>{console.log(`Server listening to 8000`)})
